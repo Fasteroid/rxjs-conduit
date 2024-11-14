a85ebe2d0afcdf246ed5357035e8ad84ae70572a
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2024-11-14 13:18:30 -0500

    derived angular implementation

--- src/angular/index.ts
+++ src/angular/index.ts
@@ -1,5 +1,5 @@
 import { DestroyRef, inject } from "@angular/core";
-import { Conduit } from "../vanilla";
+import { Conduit, ReadonlyConduit } from "../vanilla";
 
 export class NgConduit<T> extends Conduit<T> {
 
@@ -10,6 +10,21 @@ export class NgConduit<T> extends Conduit<T> {
     constructor(first?: T) {
         super(...arguments);
         inject(DestroyRef).onDestroy(() => this.complete());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static override derived<
+        Result, 
+        Sources extends {[k: string]: Conduit<any>}
+    >( 
+        sources: Sources,
+        formula: (args: { [K in keyof Sources]: Sources[K] extends Conduit<infer U> ? U : never }) => Result
+    ): ReadonlyConduit<Result> {
+        let out = Conduit.derived(sources, formula) as Conduit<Result>; // TODO: this cast is a bit of a hack
+        inject(DestroyRef).onDestroy(() => out.complete());
+        return out as ReadonlyConduit<Result>;
     }
 
 }
